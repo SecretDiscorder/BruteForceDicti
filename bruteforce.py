9775c998ea81ee98b145a02327bdee876dadd014
SecretDiscorder <139457966+SecretDiscorder@users.noreply.github.com>
2023-08-14 20:19:34 +0700

    Add files via upload

--- bruteforce.py
+++ bruteforce.py
@@ -24,7 +24,7 @@ else:
             f.write("".join(combination) + "\n")
 
     print(f"Permutations of length {length} saved to {output_file}")
- output file in write mode
+output file in write mode
     output_file = "output.txt"
     with open(output_file, "w") as f:
         for combination in permutations:
@@ -32,45 +32,4 @@ else:
             f.write("".join(combination) + "\n")
 
     print(f"Permutations of length {length} saved to {output_file}")
-    output_file2 = "output2.txt"
-    with open(output_file2, "w") as f:
-        for combination2 in permutations2:
-            # Join the characters and write to the file
-            f.write("".join(combination2) + "\n")
-
-    print(f"Permutations of length {length} saved to {output_file2}")
-    output_file3 = "output3.txt"
-    with open(output_file3, "w") as f:
-        for combination3 in permutations3:
-            # Join the characters and write to the file
-            f.write("".join(combination3) + "\n")
-
-    print(f"Permutations of length {length} saved to {output_file3}")
-    output_file4 = "output4.txt"
-    with open(output_file4, "w") as f:
-        for combination4 in permutations4:
-            # Join the characters and write to the file
-            f.write("".join(combination4) + "\n")
-
-    print(f"Permutations of length {length} saved to {output_file4}")
-    output_file5 = "output5.txt"
-    with open(output_file5, "w") as f:
-        for combination5 in permutations5:
-            # Join the characters and write to the file
-            f.write("".join(combination5) + "\n")
-
-    print(f"Permutations of length {length} saved to {output_file5}")
-    output_file6 = "output6.txt"
-    with open(output_file6, "w") as f:
-        for combination6 in permutations6:
-            # Join the characters and write to the file
-            f.write("".join(combination6) + "\n")
-
-    print(f"Permutations of length {length} saved to {output_file6}")
     
-
-
-
-    
-
-
